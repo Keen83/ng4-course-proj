b26bf935544897798beb9065ec6e2355b3544642
Olexandr Nikolaevsky <kanary@inbox.ru>
2017-06-28 12:05:43 +0300

    Add basic functionality to course project

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe',
+      'Simple test recipe description',
+      'http://media.indiatimes.in/media/content/2015/Jun/letsbecomefooddiiee-7_1434615821.jpg'),
+    new Recipe('A Test Recipe',
+      'Simple test recipe description',
+      'http://media.indiatimes.in/media/content/2015/Jun/letsbecomefooddiiee-7_1434615821.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
